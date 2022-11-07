4682e79569a432e3fe0f95130427c92672a2f733
Aaradhy Sharma <as783@snu.edu.in>
2022-11-07 16:25:29 +0530

    This is a work under progress, dont copy it rn!!

---
+++ assignments/graded assignments/assignment 8/raw codes - tbd/p5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define null 0
+
+// function declaration //
+void pointer_array_sum(int **arr[], int *sum, int *string_length);
+
+//driver code//
+
+signed main(void)
+{
+    
+}
