eae29a5f90f6faf8530538f57d68a2397888f159
Aaradhy Sharma <as783@snu.edu.in>
2022-11-07 16:29:59 +0530

    This is a work in progress, dont copy it rn!!'

--- assignments/graded assignments/assignment 8/raw codes - tbd/p5.c
+++ assignments/graded assignments/assignment 8/raw codes - tbd/p5.c
@@ -4,11 +4,37 @@
 #define null 0
 
 // function declaration //
-void pointer_array_sum(int **arr[], int *sum, int *string_length);
+void pointer_array_sum(int arr[], int *sum, int *string_length);
 
 //driver code//
 
 signed main(void)
 {
-    
+    int n;
+    printf("\n Please enter the number of elements in the array  (max 30) :");
+    scanf("%d", &n);
+    int array[n];
+    int array_sum = 0;
+    printf("\n Please enter the elements of the array : ");
+    for (signed index = 0; index < n; index++)
+        {
+            scanf("%d", &array[index]);
+        }
+
+	int array_length = sizeof(array) / sizeof(array[0]);
+	pointer_array_sum(&array[n],&array_sum,&array_length);
+
+    printf("\n The length of the string is %d", array_length);
+	return null;
+}
+
+// function definition //
+void pointer_array_sum(int arr[], int *sum, int *string_length)
+{
+    for (int i = 0; i <30; i++)
+        {
+             sum += arr[i];
+        }
+    printf("\n The sum of the array you entered is : %d", sum);
+
 }
